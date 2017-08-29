fef6739aa07d8308172d03865106f759cb4e4a9b
Teddy Chung <agv.teddyc@xl.co.id>
2017-08-29 14:07:39 +0700

    add page info and setting

---
+++ src/pages/info/info.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-info',
+  templateUrl: 'info.html',
+})
+export class InfoPage {
+
+  constructor(public navCtrl: NavController) {
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad InfoPage');
+  }
+
+}
---
+++ src/pages/setting/setting.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-setting',
+  templateUrl: 'setting.html',
+})
+export class SettingPage {
+
+  constructor(public navCtrl: NavController) {
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad SettingPage');
+  }
+
+}
