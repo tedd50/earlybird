7467f02a9752941c6f32d43d55feb6b661e40538
Husin Wijaya <hw@mbp>
2017-08-30 12:06:17 +0700

    add timer conter

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, NgZone } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { DatabaseServiceProvider } from '../../providers/database-service/database-service';
 import moment from 'moment';
@@ -9,13 +9,21 @@ import moment from 'moment';
 })
 export class HomePage {
   userId: any
-  currentDate:String
-  currentTime:String
+  currentDate: String
+  currentTime: String
+  
   constructor(public navCtrl: NavController,
-    private databaseService: DatabaseServiceProvider) {
-      databaseService.login().then((userId:String)=>this.userId=userId)
-      this.currentDate=moment().format('dddd, DD MMMM YYYY')
-      this.currentTime=moment().format('h:mm:ss a')
+    private databaseService: DatabaseServiceProvider,
+    private zone: NgZone) {
+    databaseService.login().then((userId: String) => this.userId = userId)
+    zone.runOutsideAngular(()=>{
+      this.updateTime()
+    })
+  }
+  updateTime() {
+    this.currentDate = moment().format('dddd, DD MMMM YYYY')
+    this.currentTime = moment().format('h:mm:ss a')
+    setTimeout(()=>this.updateTime(),1000)
   }
   checkIn() {
     this.databaseService.checkIn()
